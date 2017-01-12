3ac535b8f21869397be34b6977ecf5e56905968c
Brandon Brown <brandon@egovtn.org>
2017-01-12 15:57:57 -0600

    Initial project check in after week 1 lecture

---
+++ src/main/java/com/mongodb/HelloWorldSparkStyle.java
@@ -0,0 +1,10 @@
+package com.mongodb;
+
+import static spark.Spark.get;
+
+
+public class HelloWorldSparkStyle {
+    public static void main(String[] args) {
+        get("/hello",(req, res) -> "Hello World from Spark");
+    }
+}
